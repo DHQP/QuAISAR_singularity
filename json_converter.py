aca6a6f59bc22f1586937643339b10df6aa09c0e
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-02-18 13:58:09 -0500

    sic?

--- json_converter.py
+++ json_converter.py
@@ -26,7 +26,7 @@ def convert_json_to_text(infile, outfile):
     with open(infile, 'r') as f:
         results = json.load(f)
 
-    print(results['plasmidfinder']['results'])
+    print(results['plasmidfinder']['results']['Enterobacteriaceae'])
 
 args = parseArgs()
 convert_json_to_text(args.input, args.output)
