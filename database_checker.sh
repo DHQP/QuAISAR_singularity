f73b4ca84aa58d38fb8b05d6caeb8a098b3f1586
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-13 15:55:28 -0400

    trying different method
    Former-commit-id: ff70198848f9245c20757c1c2f567561222dc499

--- database_checker.sh
+++ database_checker.sh
@@ -42,6 +42,7 @@ fi
 # Check for BUSCO
 if [[ ! -d "${local_DBs}/BUSCO" ]]; then
 	mkdir -p "${local_DBs}/BUSCO"
+	cd "${local_DBs}/BUSCO"
 	# Check for top level bacteria database
 	if [[ ! -d "${local_DBs}/bacteria_odb10.2019-06-26" ]]; then
 		wget -P "${local_DBs}/BUSCO" "http://busco-data.ezlab.org/v4/data/lineages/bacteria_odb10.2019-06-26.tar.gz"
@@ -134,20 +135,21 @@ if [[ ! -d "${local_DBs}/BUSCO" ]]; then
 	if [[ ! -d "${local_DBs}/actinobacteria_class_odb10.2019-04-24" ]]; then
 		wget -P "${local_DBs}/BUSCO" "http://busco-data.ezlab.org/v4/data/lineages/actinobacteria_class_odb10.2019-04-24.tar.gz"
 	fi
-	chmod 777 ${local_DBs}/BUSCO/*.tar.gz
-	tar xzf ${local_DBs}/BUSCO/*.tar.gz --directory ${local_DBs}/BUSCO
+	chmod 777 *.tar.gz
+	tar xzf *.tar.gz
 fi
 
 ##### Currently down.....and has been a while
 # Check to see if gottcha database is installed
  if [[ ! -d "${local_DBs}/gottcha" ]]; then
  	mkdir -p "${local_DBs}/gottcha"
+	cd "${local_DBs}/gottcha"
 	# Original LANL hosted address that has been down a good while
  	#wget -P "${local_DBs}/gottcha" "https://edge-dl.lanl.gov/gottcha/GOTTCHA_database_v20150825/GOTTCHA_BACTERIA_c4937_k24_u30_xHUMAN3x.species.tar.gz"
 	# Temporary mirror until original is fixed
 	wget -O ${local_DBs}/gottcha/GOTTCHA_BACTERIA_c4937_k24_u30_xHUMAN3x.species.tar.gz "${local_DBs}/gottcha" "https://zenodo.org/record/819341/files/gottcha_bac_arc_v1.tar.gz?download=1"
-	chmod 777 "${local_DBs}/gottcha/GOTTCHA_BACTERIA_c4937_k24_u30_xHUMAN3x.species.tar.gz"
-	tar xzf "${local_DBs}/gottcha/GOTTCHA_BACTERIA_c4937_k24_u30_xHUMAN3x.species.tar.gz" --directory ${local_DBs}/gottcha
+	chmod 777 "GOTTCHA_BACTERIA_c4937_k24_u30_xHUMAN3x.species.tar.gz"
+	tar xzf "GOTTCHA_BACTERIA_c4937_k24_u30_xHUMAN3x.species.tar.gz"
  fi
 
 # # Possible solution to gottchaV1 not working
@@ -161,9 +163,10 @@ fi
 # Check to see if kraken mini database is installed
 if [[ ! -d "${local_DBs}/kraken" ]]; then
 	mkdir -p "${local_DBs}/kraken"
+	cd "${local_DBs}/kraken"
 	wget -P "${local_DBs}/kraken" "https://ccb.jhu.edu/software/kraken/dl/minikraken_20171019_4GB.tgz"
-	chmod 777 "${local_DBs}/kraken/minikraken_20171019_4GB.tgz"
-	tar xzf "${local_DBs}/kraken/minikraken_20171019_4GB.tgz" --directory ${local_DBs}/kraken
+	chmod 777 minikraken_20171019_4GB.tgz
+	tar xzf minikraken_20171019_4GB.tgz
 fi
 
 # All other databases will need to be hosted somehwere before being able to be checked/updated. Currently they are included in the Docker image
