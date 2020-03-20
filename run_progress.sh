5be012f5c22cd01305a632d88a625c20c6a3134e
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-20 11:01:49 -0400

    missed info now there and then
    Former-commit-id: 618921961a089c285e9e149adac3e2e7572aa0c7

--- run_progress.sh
+++ run_progress.sh
@@ -63,9 +63,9 @@ BAR=$(printf "%${BAR_length}s" | tr ' ' $BAR_character)
 	echo "${current_Isolate_progress}"
 	echo "${total_progress}"
 	echo -ne "\r${BAR:0:$current_Isolate_progress}(${current_Isolate_progress}%-${current_Isolate_name})"
-	echo -ne "\r${BAR:0:$total_progress}"
+	echo -ne "\r${BAR:0:$total_progress}(${total_progress}%)"
 	sleep 1
-	if [[ "${total_progress}" -eq 100 ]];
+	if [[ "${total_progress}" -eq 100 ]]; then
 		echo "Run is complete!!!"
 		exit
 	fi
