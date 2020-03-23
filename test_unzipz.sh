48192ff0655bc3759db8e9ed16279441a2f5a735
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-23 15:44:37 -0400

    test for ANI unzipping
    Former-commit-id: 94eccf2044e6b10e48c1b28f92864b5a1f879b9e

---
+++ test_unzipz.sh
@@ -0,0 +1,11 @@
+for i in ${SAMPDATADIR}/ANI/localANIDB_REFSEQ/*.gz; do
+  old_name=$(basename ${i} | rev | cut -d'.' -f2- | rev)
+  new_name=$(echo ${old_name} | tr -d '[],')
+  dir_name=$(dirname ${i})
+  echo -e "${i}\n${old_name}\n${new_name}\n${dir_name}"
+  gunzip ${i}
+  tax_genus=$(head -n1 "${dir_name}/${old_name}" | cut -d' ' -f2 | tr -d '[],')
+  tax_species=$(head -n1 "${dir_name}/${old_name}" | cut -d' ' -f3 | tr -d '[],')
+  echo "Taxes: ${tax_genus}:${tax_species}"
+  mv ${dir_name}/${old_name}.fna ${dir_name}/${tax_genus}_${tax_species}_${new_name}.fasta
+done < "${1}"
