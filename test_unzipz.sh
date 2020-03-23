036ea98f6037c734751fb99c248cb958aa831090
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-23 16:28:05 -0400

    git it
    Former-commit-id: cc35e438a94e2c3818100adc80777fb88149d6e9

--- test_unzipz.sh
+++ test_unzipz.sh
@@ -1,11 +1,11 @@
 for i in ${1}/*.gz; do
-  old_name=$(basename ${i} | rev | cut -d'.' -f3- | rev)
-  new_name=$(echo ${old_name} | tr -d '[],').fasta
+  old_name=$(basename ${i} | cut -d'.' -f1)
+  new_name=$(echo ${old_name} | tr -d '[],')
   dir_name=$(dirname ${i})
-  echo -e "${i}\n${old_name}\n${new_name}\n${dir_name}"
+  echo -e "${i}\n${old_name}.fna\n${new_name}.fasta\n${dir_name}"
   gunzip ${i}
-  tax_genus=$(head -n1 "${dir_name}/${old_name}" | cut -d' ' -f2 | tr -d '[],')
-  tax_species=$(head -n1 "${dir_name}/${old_name}" | cut -d' ' -f3 | tr -d '[],')
+  tax_genus=$(head -n1 "${dir_name}/${old_name}.fna" | cut -d' ' -f2 | tr -d '[],')
+  tax_species=$(head -n1 "${dir_name}/${old_name}.fna" | cut -d' ' -f3 | tr -d '[],')
   echo "Taxes: ${tax_genus}:${tax_species}"
-  mv ${dir_name}/${old_name} ${dir_name}/${tax_genus}_${tax_species}_${new_name}
+  mv ${dir_name}/${old_name}.fna ${dir_name}/${tax_genus}_${tax_species}_${new_name}.fasta
 done < "${1}"
