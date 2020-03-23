73fd34758a8d950e815a21fea6c52dbcec3dcb2f
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-23 16:16:26 -0400

    retry all
    Former-commit-id: 9d9b30d6b4ff07f1ddabbbd76f8ea3c59728266a

--- test_unzipz.sh
+++ test_unzipz.sh
@@ -1,12 +1,11 @@
 for i in ${1}/*.gz; do
-  old_name=$(basename ${i} | rev | cut -d'.' -f3- | rev)
+  old_name=$(basename ${i} | rev | cut -d'.' -f2- | rev)
   new_name=$(echo ${old_name} | tr -d '[],').fasta
   dir_name=$(dirname ${i})
   echo -e "${i}\n${old_name}\n${new_name}\n${dir_name}"
-  exit
   gunzip ${i}
   tax_genus=$(head -n1 "${dir_name}/${old_name}" | cut -d' ' -f2 | tr -d '[],')
   tax_species=$(head -n1 "${dir_name}/${old_name}" | cut -d' ' -f3 | tr -d '[],')
   echo "Taxes: ${tax_genus}:${tax_species}"
-  mv ${dir_name}/${old_name}.fna ${dir_name}/${tax_genus}_${tax_species}_${new_name}.fasta
+  mv ${dir_name}/${old_name} ${dir_name}/${tax_genus}_${tax_species}_${new_name}
 done < "${1}"
