54438f337c423f14a8d2616f30328caa40e53a28
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-24 15:35:51 -0400

    aded refseq for clarity
    Former-commit-id: 40f36fe87665498e4dea4dd033b1910b622e3a21

--- validate_piperun.sh
+++ validate_piperun.sh
@@ -873,7 +873,7 @@ fi
 #Check ANI
 ani_found=false
 #Check ANI REFSEQ. Not fully implemented yet, so not causing a failure in reporting
-if [[ -f "${OUTDATADIR}/ANI/best_ANI_hits_ordered(${1}_vs_${REFSEQ_date}).txt" ]]; then
+if [[ -f "${OUTDATADIR}/ANI/best_ANI_hits_ordered(${1}_vs_REFSEQ_${REFSEQ_date}).txt" ]]; then
 	#echo "ALL"
 	ani_info=$(head -n 1 "${OUTDATADIR}/ANI/best_ANI_hits_ordered(${1}_vs_${REFSEQ_date}).txt")
 	percent_match=$(echo "${ani_info}" | cut -d'.' -f1)
