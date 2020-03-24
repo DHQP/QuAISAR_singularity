1405bea1bcb395e6f44a424c6ae2cf3972647fbb
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-24 13:34:10 -0400

    zip
    Former-commit-id: 81538abb787c21af7d8ac64df9c800e87b5f2898

--- test_unzipz.sh
+++ test_unzipz.sh
@@ -1,13 +1,19 @@
-for i in ${1}/*.gz; do
-  old_name=$(basename ${i} | cut -d'.' -f1,2)
-  new_name=$(echo ${old_name} | tr -d '[],')
-  dir_name=$(dirname ${i})
-  echo "${old_name}-${new_name}-${dirname}"
-  echo "unzipping ${i}"
-  gunzip ${i}
-  tax_genus=$(head -n1 "${dir_name}/${old_name}.fna" | cut -d' ' -f2 | tr -d '[],')
-  tax_species=$(head -n1 "${dir_name}/${old_name}.fna" | cut -d' ' -f3 | tr -d '[],')
-  echo "Taxes: ${tax_genus}:${tax_species}"
-  echo "Moving ${dir_name}/${old_name}.fna to ${dir_name}/${tax_genus}_${tax_species}_${new_name}.fasta"
-  mv ${dir_name}/${old_name}.fna ${dir_name}/${tax_genus}_${tax_species}_${new_name}.fasta
+echo "${1}"*.gz
+for i in ${1}/*; do
+  ext=$(echo "${i}"| rev | cut -d'.' -f1 | rev)
+  if [[ "${ext}" == "gz" ]]; then
+    old_name=$(basename ${i} | cut -d'.' -f1,2)
+    new_name=$(echo ${old_name} | tr -d '[],')
+    dir_name=$(dirname ${i})
+    echo "${old_name}-${new_name}-${dirname}"
+    echo "unzipping ${i}"
+    gunzip ${i}
+    tax_genus=$(head -n1 "${dir_name}/${old_name}.fna" | cut -d' ' -f2 | tr -d '[],')
+    tax_species=$(head -n1 "${dir_name}/${old_name}.fna" | cut -d' ' -f3 | tr -d '[],')
+    echo "Taxes: ${tax_genus}:${tax_species}"
+    echo "Moving ${dir_name}/${old_name}.fna to ${dir_name}/${tax_genus}_${tax_species}_${new_name}.fasta"
+    mv ${dir_name}/${old_name}.fna ${dir_name}/${tax_genus}_${tax_species}_${new_name}.fasta
+  else
+    echo "${i} was not a gz file"
+  fi
 done
