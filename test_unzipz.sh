36418c6f8b3b548e1bb7da5bd152f3363a1ef716
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-24 12:37:43 -0400

    output
    Former-commit-id: 9d9860eec7ae6308e8b9ba95b61f27927a21da2e

--- test_unzipz.sh
+++ test_unzipz.sh
@@ -2,9 +2,12 @@ for i in ${1}/ANI/localANIDB_REFSEQ/*.gz; do
   old_name=$(basename ${i} | cut -d'.' -f1,2)
   new_name=$(echo ${old_name} | tr -d '[],')
   dir_name=$(dirname ${i})
+  echo "${old_name}-${new_name}-${dirname}"
+  echo "unzipping ${i}"
   gunzip ${i}
   tax_genus=$(head -n1 "${dir_name}/${old_name}.fna" | cut -d' ' -f2 | tr -d '[],')
   tax_species=$(head -n1 "${dir_name}/${old_name}.fna" | cut -d' ' -f3 | tr -d '[],')
   echo "Taxes: ${tax_genus}:${tax_species}"
+  echo "Moving ${dir_name}/${old_name}.fna to ${dir_name}/${tax_genus}_${tax_species}_${new_name}.fasta"
   mv ${dir_name}/${old_name}.fna ${dir_name}/${tax_genus}_${tax_species}_${new_name}.fasta
 done
