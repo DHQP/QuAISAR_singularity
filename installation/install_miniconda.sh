5a369f250c220055f71b4214dfd065518ebd248b
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-26 15:53:19 -0400

    Attempting to try different singularity version with correct output
    Former-commit-id: dd5403add83ac1d31f13e0f1760ddb6bd053fac6

--- installation/install_miniconda.sh
+++ installation/install_miniconda.sh
@@ -2,12 +2,17 @@
 cd ~
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
+
+# Didnt work on my account CERES
 echo 'export PATH="~/miniconda3/bin:$PATH"' >> ~/.bashrc
+
+sleep 2
+
 . ~/.bashrc
 conda init bash
 conda config --set auto_activate_base false
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
-conda install -c conda-forge singularity=3.5.3
+conda install -c conda-forge/label/cf202003 singularity
 rm Miniconda3-latest-Linux-x86_64.sh
