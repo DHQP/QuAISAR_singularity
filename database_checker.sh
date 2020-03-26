eec530f9ebc11aa2ce3a752faa1aa2e7ce89c4e5
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-26 10:35:37 -0400

    Changed way actinobacteria is named in DB
    Former-commit-id: 15f1729010639fb4e9cbc7a09b68c3807c449040

--- database_checker.sh
+++ database_checker.sh
@@ -76,6 +76,10 @@ for odb_info in "${busco_taxa[@]}"; do
 			fi
 			echo "Downloading latest BUSCO database for ${taxa} (wget http://busco-data.ezlab.org/v4/data/lineages/${taxa}_odb10.${db_date}.tar.gz)"
 			wget "http://busco-data.ezlab.org/v4/data/lineages/${taxa}_odb10.${db_date}.tar.gz"
+			# Dont know how to handle this one outlier (only one to specify a level in the filename) - ALl OUR bugs are in class Actinobacteria too
+			if [[ "${taxa}" == "actinobacteria" ]]; then
+				mv ${local_DBs}/BUSCO/actinobacteria_class_odb10.2019-04-24.tar.gz ${local_DBs}/BUSCO/actinobacteria_odb10.2019-04-24.tar.gz
+			fi
 		else
 			echo "Missing latest BUSCO database for ${taxa}"
 			missing_DBS=("${missing_DBS[@]}" "BUSO-${taxa}")
