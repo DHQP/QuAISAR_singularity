459bee690f3aff3c7bd4e8740de013a14774cee0
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-27 14:34:29 -0400

    wasnt working in install pipeline, retrying why
    Former-commit-id: 3de0bac0d564708f8324f0332d5bfd3ab93346d4

--- installation/install_miniconda.sh
+++ installation/install_miniconda.sh
@@ -3,14 +3,9 @@ cd ~
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
 
-# Didnt work on my account CERES
-echo 'export PATH="~/miniconda3/bin:$PATH"' >> ~/.bashrc
-
-sleep 2
-
-. ~/.bashrc
+#. ~/.bashrc
 conda init bash
-conda config --set auto_activate_base true
+conda config --set auto_activate_base false
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
