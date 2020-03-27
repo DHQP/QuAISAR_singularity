ed8f1f06e1204a9d85746f596b06c6314078213d
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-03-27 14:43:13 -0400

    trying specific call
    Former-commit-id: 134021ea8dbd63dc5427f42d2eae70a5ad554a05

--- installation/install_miniconda.sh
+++ installation/install_miniconda.sh
@@ -4,7 +4,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
 
 #. ~/.bashrc
-conda init bash
+~/miniconda/conda/bin init bash
 conda config --set auto_activate_base false
 conda config --add channels defaults
 conda config --add channels bioconda
