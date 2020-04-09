a7272612d802d81f73370093dc6894a428dd29c8
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-04-09 12:10:43 -0400

    removde BUSCO, gottcha, and kraken for testing purposes
    Former-commit-id: b24360ea0390cf203b0d65314a8b2a4bab1bb5e8

--- scripts/database_checker.sh
+++ scripts/database_checker.sh
@@ -68,39 +68,39 @@ if [[ ! -d ${local_DBs} ]]; then
 	mkdir -p ${local_DBs}
 fi
 
-# # Check for BUSCO
-busco_taxa=(bacteria_odb10.2019-06-26 alteromonadales_odb10.2019-04-24 bacillales_odb10.2019-04-24 bacilli_odb10.2019-04-24 bacteroidetes_odb10.2019-04-24 betaproteobacteria_odb10.2019-04-24 burkholderiales_odb10.2019-04-24 campylobacterales_odb10.2019-04-24 clostridiales_odb10.2019-04-24 clostridia_odb10.2019-04-24 corynebacteriales_odb10.2019-04-24 enterobacterales_odb10.2019-04-24 epsilonproteobacteria_odb10.2019-04-24 firmicutes_odb10.2019-04-24 flavobacteriales_odb10.2019-04-24  flavobacteriia_odb10.2019-04-24 gammaproteobacteria_odb10.2019-04-24 lactobacillales_odb10.2019-04-24 neisseriales_odb10.2019-04-24 proteobacteria_odb10.2019-04-24 pseudomonadales_odb10.2019-04-24 xanthomonadales_odb10.2019-04-24 actinobacteria_class_odb10.2019-04-24 )
-
-#echo "${#busco_taxa[@]}"
-
-for odb_info in "${busco_taxa[@]}"; do
-	# Check for top level bacteria database
-	#echo ${odb_info}
-	taxa=$(echo "$odb_info" | cut -d'_' -f1)
-	db_date=$(echo "$odb_info" | cut -d'.' -f2)
-	if [[ ! -d "${local_DBs}/BUSCO/${taxa}_odb10" ]]; then
-		if [[ "${do_download}" = "true" ]]; then
-			if [[ ! -d "${local_DBs}/BUSCO" ]]; then
-				mkdir "${local_DBs}/BUSCO"
-			fi
-			cd "${local_DBs}/BUSCO"
-			if [[ "${taxa}" == "actinobacteria" ]]; then
-				taxa="actinobacteria_class"
-			fi
-			echo "Downloading latest BUSCO database for ${taxa} (wget http://busco-data.ezlab.org/v4/data/lineages/${taxa}_odb10.${db_date}.tar.gz)"
-			wget "http://busco-data.ezlab.org/v4/data/lineages/${taxa}_odb10.${db_date}.tar.gz"
-			# Dont know how to handle this one outlier (only one to specify a level in the filename) - ALl OUR bugs are in class Actinobacteria too
-		else
-			echo "Missing latest BUSCO database for ${taxa}"
-			missing_DBS=("${missing_DBS[@]}" "BUSCO-${taxa}")
-		fi
-	else
-		echo "BUSCO has latest ${taxa}_odb10 as of 3/15/2020"
-	fi
-done
-find ${local_DBs}/BUSCO/ -name '*.gz' -exec tar xzf {} \;
-mv ${local_DBs}/BUSCO/actinobacteria_class_odb10 ${local_DBs}/BUSCO/actinobacteria_odb10
-find ${local_DBs}/BUSCO/ -name '*.gz' -exec rm {} \;
+# # # Check for BUSCO
+# busco_taxa=(bacteria_odb10.2019-06-26 alteromonadales_odb10.2019-04-24 bacillales_odb10.2019-04-24 bacilli_odb10.2019-04-24 bacteroidetes_odb10.2019-04-24 betaproteobacteria_odb10.2019-04-24 burkholderiales_odb10.2019-04-24 campylobacterales_odb10.2019-04-24 clostridiales_odb10.2019-04-24 clostridia_odb10.2019-04-24 corynebacteriales_odb10.2019-04-24 enterobacterales_odb10.2019-04-24 epsilonproteobacteria_odb10.2019-04-24 firmicutes_odb10.2019-04-24 flavobacteriales_odb10.2019-04-24  flavobacteriia_odb10.2019-04-24 gammaproteobacteria_odb10.2019-04-24 lactobacillales_odb10.2019-04-24 neisseriales_odb10.2019-04-24 proteobacteria_odb10.2019-04-24 pseudomonadales_odb10.2019-04-24 xanthomonadales_odb10.2019-04-24 actinobacteria_class_odb10.2019-04-24 )
+#
+# #echo "${#busco_taxa[@]}"
+#
+# for odb_info in "${busco_taxa[@]}"; do
+# 	# Check for top level bacteria database
+# 	#echo ${odb_info}
+# 	taxa=$(echo "$odb_info" | cut -d'_' -f1)
+# 	db_date=$(echo "$odb_info" | cut -d'.' -f2)
+# 	if [[ ! -d "${local_DBs}/BUSCO/${taxa}_odb10" ]]; then
+# 		if [[ "${do_download}" = "true" ]]; then
+# 			if [[ ! -d "${local_DBs}/BUSCO" ]]; then
+# 				mkdir "${local_DBs}/BUSCO"
+# 			fi
+# 			cd "${local_DBs}/BUSCO"
+# 			if [[ "${taxa}" == "actinobacteria" ]]; then
+# 				taxa="actinobacteria_class"
+# 			fi
+# 			echo "Downloading latest BUSCO database for ${taxa} (wget http://busco-data.ezlab.org/v4/data/lineages/${taxa}_odb10.${db_date}.tar.gz)"
+# 			wget "http://busco-data.ezlab.org/v4/data/lineages/${taxa}_odb10.${db_date}.tar.gz"
+# 			# Dont know how to handle this one outlier (only one to specify a level in the filename) - ALl OUR bugs are in class Actinobacteria too
+# 		else
+# 			echo "Missing latest BUSCO database for ${taxa}"
+# 			missing_DBS=("${missing_DBS[@]}" "BUSCO-${taxa}")
+# 		fi
+# 	else
+# 		echo "BUSCO has latest ${taxa}_odb10 as of 3/15/2020"
+# 	fi
+# done
+# find ${local_DBs}/BUSCO/ -name '*.gz' -exec tar xzf {} \;
+# mv ${local_DBs}/BUSCO/actinobacteria_class_odb10 ${local_DBs}/BUSCO/actinobacteria_odb10
+# find ${local_DBs}/BUSCO/ -name '*.gz' -exec rm {} \;
 
 # All other databases will need to be hosted somehwere before being able to be checked/updated. Currently they are included in the Docker image
 
@@ -279,49 +279,49 @@ for simage_info in "${singularities[@]}"; do
 	chmod 755 ${local_DBs}/singularities/*
 done
 
-# Check to see if kraken mini database is installed
-if [[ ! -d "${local_DBs}/kraken" ]]; then
-	if [[ "${do_download}" = "true" ]]; then
-		mkdir "${local_DBs}/kraken"
-		cd "${local_DBs}/kraken"
-		echo "Downloading latest (mini)kraken database (wget https://ccb.jhu.edu/software/kraken/dl/minikraken_20171019_4GB.tgz)"
-		wget "https://ccb.jhu.edu/software/kraken/dl/minikraken_20171019_4GB.tgz"
-		if [[ ! -f "minikraken_20171019_4GB.tgz" ]]; then
-			curl -O "https://ccb.jhu.edu/software/kraken/dl/minikraken_20171019_4GB.tgz"
-		fi
-		tar xzf minikraken_20171019_4GB.tgz
-		rm minikraken_20171019_4GB.tgz
-	else
-		echo "Missing latest kraken database"
-		missing_DBS=("${missing_DBS[@]}" "kraken")
-	fi
-else
-	echo "kraken database is installed"
-fi
-
-##### Currently down.....and has been a while
-# Check to see if gottcha database is installed
-if [[ ! -d "${local_DBs}/gottcha" ]]; then
-	if [[ "${do_download}" = "true" ]]; then
-		cd "${local_DBs}"
-		# Original LANL hosted address that has been down a good while
-	 	#wget -P "${local_DBs}/gottcha" "https://edge-dl.lanl.gov/gottcha/GOTTCHA_database_v20150825/GOTTCHA_BACTERIA_c4937_k24_u30_xHUMAN3x.species.tar.gz"
-		# Temporary mirror until original is fixed
-		echo "Downloading latest gottcha database (wget https://zenodo.org/record/819341/files/gottcha_bac_arc_v1.tar.gz)"
-		wget "https://zenodo.org/record/819341/files/gottcha_bac_arc_v1.tar.gz"
-		tar xzf gottcha_bac_arc_v1.tar.gz
-		rm gottcha_bac_arc_v1.tar.gz
-		mv gottcha/gottcha_db ./
-		rm -r gottcha
-		mv gottcha_db gottcha
-		rm gottcha.dbprofile.out
-		# Need to find sa place to host genus_Lookup.tar.gz
-	else
-		echo "Missing gottcha database"
-		missing_DBS=("${missing_DBS[@]}" "gottcha")
-	fi
-else
-	echo "gottcha database installed"
-fi
+# # Check to see if kraken mini database is installed
+# if [[ ! -d "${local_DBs}/kraken" ]]; then
+# 	if [[ "${do_download}" = "true" ]]; then
+# 		mkdir "${local_DBs}/kraken"
+# 		cd "${local_DBs}/kraken"
+# 		echo "Downloading latest (mini)kraken database (wget https://ccb.jhu.edu/software/kraken/dl/minikraken_20171019_4GB.tgz)"
+# 		wget "https://ccb.jhu.edu/software/kraken/dl/minikraken_20171019_4GB.tgz"
+# 		if [[ ! -f "minikraken_20171019_4GB.tgz" ]]; then
+# 			curl -O "https://ccb.jhu.edu/software/kraken/dl/minikraken_20171019_4GB.tgz"
+# 		fi
+# 		tar xzf minikraken_20171019_4GB.tgz
+# 		rm minikraken_20171019_4GB.tgz
+# 	else
+# 		echo "Missing latest kraken database"
+# 		missing_DBS=("${missing_DBS[@]}" "kraken")
+# 	fi
+# else
+# 	echo "kraken database is installed"
+# fi
+#
+# ##### Currently down.....and has been a while
+# # Check to see if gottcha database is installed
+# if [[ ! -d "${local_DBs}/gottcha" ]]; then
+# 	if [[ "${do_download}" = "true" ]]; then
+# 		cd "${local_DBs}"
+# 		# Original LANL hosted address that has been down a good while
+# 	 	#wget -P "${local_DBs}/gottcha" "https://edge-dl.lanl.gov/gottcha/GOTTCHA_database_v20150825/GOTTCHA_BACTERIA_c4937_k24_u30_xHUMAN3x.species.tar.gz"
+# 		# Temporary mirror until original is fixed
+# 		echo "Downloading latest gottcha database (wget https://zenodo.org/record/819341/files/gottcha_bac_arc_v1.tar.gz)"
+# 		wget "https://zenodo.org/record/819341/files/gottcha_bac_arc_v1.tar.gz"
+# 		tar xzf gottcha_bac_arc_v1.tar.gz
+# 		rm gottcha_bac_arc_v1.tar.gz
+# 		mv gottcha/gottcha_db ./
+# 		rm -r gottcha
+# 		mv gottcha_db gottcha
+# 		rm gottcha.dbprofile.out
+# 		# Need to find sa place to host genus_Lookup.tar.gz
+# 	else
+# 		echo "Missing gottcha database"
+# 		missing_DBS=("${missing_DBS[@]}" "gottcha")
+# 	fi
+# else
+# 	echo "gottcha database installed"
+# fi
 
 echo "There are ${#missing_DBS[@]} missing databases (${missing_DBS[@]})"
