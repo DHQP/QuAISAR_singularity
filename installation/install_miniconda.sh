f2b04c2197b73856de54bb93e4d54e6d758cf32f
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-05-04 11:42:02 -0400

    First test of Go and singularity installation edits

--- installation/install_miniconda.sh
+++ installation/install_miniconda.sh
@@ -2,13 +2,9 @@
 cd ~
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
-
-#. ~/.bashrc
-~/miniconda3/bin/conda init bash
+echo 'export PATH="~/miniconda3/bin:$PATH"' >> ~/.bashrc
 . ~/.bashrc
+conda init bash
 conda config --set auto_activate_base false
-conda config --add channels defaults
-conda config --add channels bioconda
-conda config --add channels conda-forge
-conda install -c conda-forge singularity
+. ~/.bashrc
 rm Miniconda3-latest-Linux-x86_64.sh
--- installation/installation.sh
+++ installation/installation.sh
@@ -5,7 +5,7 @@
 #
 # Description: Script to prepare environment to be able to run quaisar pipeline
 #
-# Usage: ./quaisar_installation.sh location_for
+# Usage: ./quaisar_installation.sh OS_Type location_for_scriopts location_to_download_databases_to location_of_where_to_store_output
 #
 # Output location: No output created
 #
@@ -23,9 +23,16 @@ echo "${install_script_dir}"
 
 # Checking for proper number of arguments from command line
 if [[ $# -lt 1  || $# -gt 3 ]]; then
-  echo "Usage: ./installation.sh script_installation_location database_installation_location Working_directory_of_output_from_runs"
+  echo "Usage: ./installation.sh OS-Type script_installation_location database_installation_location Working_directory_of_output_from_runs"
 	echo "You have used $# args"
+  echo "Options for OS-type are 0-Other/No Singularity Install needed 1-Ubuntu/Debian Family, 2-CentOS/Red Hat Family"
   exit 3
+fi
+
+echo "Installing quaisar scripts to ${installation_location}"
+if [[ ! -d ${installation_location} ]]; then
+  echo "Creating ${installation_location}"
+  mkdir -p ${installation_location}
 fi
 
 # Checks for proper argumentation
@@ -36,34 +43,106 @@ elif [[ "$1" = "-h" ]]; then
 	echo "Usage: ./installation.sh script_installation_location database_installation_location Working_directory_of_output_from_runs"
   exit 0
 elif [ -z "$1" ]; then
+  echo "Empty OS_type supplied to supplied to $0, exiting"
+  exit 1
+elif [ -z "$2" ]; then
 	echo "Empty script location supplied to supplied to $0, exiting"
 	exit 1
-elif [ -z "$2" ]; then
+elif [ -z "$3" ]; then
 	echo "Empty database installation location supplied to $0, exiting"
 	exit 1
-elif [ -z "$3" ]; then
+elif [ -z "$4" ]; then
   echo "Empty working directory for output supplied to $0, exiting"
   exit 1
 fi
 
+function test_go() {
+  current_dir=$(pwd)
+  "Testing Go!"
+  echo -e "package main\n" > ${installation_location}/hello.go
+  echo -e "import fmt\n" >> ${installation_location}/hello.go
+  echo -e "func main() {\nfmt.Printf(\"hello, world\")\n}" >> ${installation_location}/hello.go
+  go build ${installation_location}/hello.go
+  cd ${installation_location}
+  go_test_output=$(go hello)
+  if [[ "${go_test_output}" = "hello, world" ]]; then
+    echo "Go is installed correctly, proceed!"
+  else
+    echo "Go is not installed, halting"
+    exit 99
+  fi
+  cd ${current_dir}
+}
 
+function test_singularity() {
+  sing_version=$(singularity --version)
+  if [[ "${sing_version}" = "singularity version"* ]]; then
+    v3plus=$(echo "${sing_version}" | cut -d' ' -f3 | cut -d'.' -f1)
+    if [[ "${v3plus}" -gt 3 ]]; then
+      echo "Singularity installed and ready to go"
+    else
+      echo "Your singularity version is too old, please upgrade to version 3 (or remove old versions and change parameters) before trying again"
+      exit
+    fi
+  else
+    echo "You do not have singularity installed, either choose a supported operating system, or preinstall before trying to install QuAISAR"
+    exit 101
+  fi
+}
 
 # need to add to bottom of yaml with proper home location
-installation_location=${1}
-databases=${2}
-working_directory=${3}
+OS_type=${1}
+installation_location=${2}
+databases=${3}
+working_directory=${4}
 
-echo "Installing quaisar scripts to ${installation_location}"
-if [[ ! -d ${installation_location} ]]; then
-  echo "Creating ${installation_location}"
-  mkdir -p ${installation_location}
-fi
+#Check if Go is installed already
+
+
+
+
+
 
 echo "Checking for working directory of runs ${working_directory}"
 if [[ ! -d ${working_directory} ]]; then
   echo "Creating ${working_directory}"
   mkdir -p ${working_directory}
 fi
+
+if [[ ${OS_type} -eq 1 ]]; then
+  echo "Installing pre-dependencies"
+  sudo apt-get update && sudo apt-get install -y \
+    build-essential \
+    libssl-dev \
+    uuid-dev \
+    libgpgme11-dev \
+    squashfs-tools \
+    libseccomp-dev \
+    wget \
+    pkg-config \
+    git \
+    cryptsetup
+  echo "Installing Go"
+  curl -O https://storage.googleapis.com/golang/go1.14.2.linux-amd64.tar.gz --output ${installation_location}
+  tar -C /usr/local -xvf go1.14.2.linux-amd64.tar.gz
+  echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+  test_go
+  echo "Installing Singularity"
+  export VERSION=3.5.2 && # adjust this as necessary \
+    wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
+    tar -C "${installation_location}" -xzf singularity-${VERSION}.tar.gz && \
+    cd ${installation_location}/singularity
+  ./mconfig && \
+    make -c builddir && \
+    sudo make -C builddir install.
+  test_singularity
+
+#Install Singularity on Redhat based systems
+elif [[ ${OS_type} -eq 2]]; then
+
+fi
+
+
 
 # Create primary config file
 head -n25 ${install_script_dir}/installation/config_template.sh > ${installation_location}/new_config.sh
@@ -83,6 +162,7 @@ rm ${installation_location}/config.sh
 mv ${installation_location}/new_config.sh ${installation_location}/config.sh
 chmod +x ${installation_location}/*
 
+echo "Exiting prematurely to prevent constant installations"
 
 # Install databases
 #Create database folder
@@ -102,10 +182,10 @@ else
 fi
 
 home_dir=$(echo $HOME)
-echo "prefix: ${home_dir}/miniconda3/envs/py36_biopython_singularity" >> ${install_script_dir}/installation/py36_biopython_singularity.yml
+echo "prefix: ${home_dir}/miniconda3/envs/py36_biopython" >> ${install_script_dir}/installation/py36_biopython.yml
 
 . "${home_dir}/.bashrc"
 
-conda create --name py36_biopython_singularity python=3.6 singularity biopython -y
+conda create --name py36_biopython python=3.6 biopython -y
 
 echo -e "Installation complete. Before running the pipeline, open a new window or run the following:\n. ${home_dir}/.bashrc \n\n If you would like to view or edit parameters and settings for pipeline, please see ${installation_location}/config.sh"
