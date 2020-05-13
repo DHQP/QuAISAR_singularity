159059ce49383e8d1224e77c5d7be211b2138719
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-05-13 19:26:29 -0400

    Hopefully last change for getting output

--- scripts/determine_taxID.sh
+++ scripts/determine_taxID.sh
@@ -150,7 +150,7 @@ do_ANI() {
 	echo "${source}"
 
 	source ./get_latest_DBs.sh ${databases}
-	refseq_ANI_date=get_ANI_REFSEQ_Date
+	refseq_ANI_date=$(get_ANI_REFSEQ_Date)
 	echo "${refseq_ANI_date}"
 	if [[ -f "${output_dir}/${project}/${sample}/ANI/best_ANI_hits_ordered(${sample}_vs_REFSEQ_${refseq_ANI_date}).txt" ]]; then
 		source_file="${output_dir}/${project}/${sample}/ANI/best_ANI_hits_ordered(${sample}_vs_REFSEQ_${refseq_ANI_date}).txt"
