7a31a48dfe781668f159050a92d6dd1153970ca6
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-08-03 13:16:41 -0400

    minor cleanup

--- scripts/quaisar_singularity.sh
+++ scripts/quaisar_singularity.sh
@@ -29,7 +29,7 @@ isolate_number=0
 isolate_count=0
 
 version_type="Quaisar-Singularity"
-version_num="qs1.0.2"
+version_num="qs1.0.3"
 
 # Will be called throughout the script to write current progress for inquisitive minds and to restart run from where it was murdered
 # parameters need to be as follows
