d024ce01d1f73fe73fe681aabbed826d9a5fe8a7
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-08-03 13:23:54 -0400

    better locale identification

--- scripts/determine_taxID_config.sh
+++ scripts/determine_taxID_config.sh
@@ -146,7 +146,8 @@ do_ANI() {
 	source="ANI"
 	#echo "${source}"
 
-	source ./get_latest_DBs.sh ${databases}
+	SCRIPT_DIR=$(dirname "$(readlink -f "$0")") # get dir of current script
+  source ${SCRIPT_DIR}/get_latest_DBs.sh ${databases}
 	refseq_ANI_date=$(get_ANI_REFSEQ_Date)
 	#echo "${refseq_ANI_date}"
 	if [[ -f "${output_dir}/${project}/${sample}/ANI/best_ANI_hits_ordered(${sample}_vs_REFSEQ_${refseq_ANI_date}).txt" ]]; then
