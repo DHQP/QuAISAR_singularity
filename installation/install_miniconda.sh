f2022b85695255c424eed29f5023a737bb25f546
Vlachos, Nicholas (CDC) <nvx4@cdc.gov>
2021-05-07 18:26:25 +0000

    Update install_miniconda.sh trying better access to conda?

--- installation/install_miniconda.sh
+++ installation/install_miniconda.sh
@@ -3,7 +3,8 @@ cd ~
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
 echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
-. ~/.bashrc
+PATH=${HOME}/miniconda3/bin:$PATH
+#. ~/.bashrc
 conda init bash
 conda config --set auto_activate_base false
 . ~/.bashrc
