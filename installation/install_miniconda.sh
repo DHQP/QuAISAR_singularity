f1fa5fc466ae201e1ef924fe02b8303858132cf0
Vlachos, Nicholas (CDC) <nvx4@cdc.gov>
2021-05-07 19:05:35 +0000

    Update install_miniconda.sh removing push to bashrc path as it isnt continuing downstream

--- installation/install_miniconda.sh
+++ installation/install_miniconda.sh
@@ -2,7 +2,7 @@
 cd ~
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
-echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
+#echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
 PATH=${HOME}/miniconda3/bin:$PATH
 #. ~/.bashrc
 conda init bash
