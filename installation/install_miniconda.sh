80c9e315c149b548f2b86634cd50cea660931327
Vlachos, Nicholas (CDC) <nvx4@cdc.gov>
2021-05-07 15:52:06 +0000

    Update install_miniconda.sh ensured installation location matches the quaisar passed var and that the bash export matches

--- installation/install_miniconda.sh
+++ installation/install_miniconda.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd ~
+cd $1
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
 echo 'export PATH="$1/miniconda3/bin:$PATH"' >> ~/.bashrc
